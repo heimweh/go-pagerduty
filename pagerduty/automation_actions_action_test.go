40afe19aeb30a05f2243e9c97bcbb4a89dab27f2
mrdubr <mr.dubr@gmail.com>
2022-12-19 16:51:37 -0500

    Add supports for Automation Actions Action Update

--- pagerduty/automation_actions_action_test.go
+++ pagerduty/automation_actions_action_test.go
@@ -157,6 +157,82 @@ func TestAutomationActionsActionTypeProcessAutomationCreate(t *testing.T) {
 	}
 }
 
+func TestAutomationActionsActionUpdate(t *testing.T) {
+	setup()
+	defer teardown()
+
+	description := "Description of Action created by TF"
+	runner_id := "01DF4O9T1MDPYOUT7SUX9EXZ4R"
+	adf_arg := "-arg 123"
+	job_id := "1519578e-a22a-4340-b58f-08194691e10b"
+	adf := AutomationActionsActionDataReference{
+		ProcessAutomationJobId:        &job_id,
+		ProcessAutomationJobArguments: &adf_arg,
+	}
+	input := &AutomationActionsAction{
+		Name:                "Action created by TF",
+		Description:         &description,
+		ActionType:          "process_automation",
+		RunnerID:            &runner_id,
+		ActionDataReference: adf,
+	}
+
+	var id = "01DF4OBNYKW84FS9CCYVYS1MOS"
+	var url = fmt.Sprintf("%s/%s", automationActionsActionBaseUrl, id)
+
+	mux.HandleFunc(url, func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "PUT")
+		v := new(AutomationActionsActionPayload)
+		json.NewDecoder(r.Body).Decode(v)
+		if !reflect.DeepEqual(v.Action, input) {
+			t.Errorf("Request body = %+v, want %+v", v.Action, input)
+		}
+		w.Write([]byte(`{"action":{"action_data_reference":{"process_automation_job_id":"1519578e-a22a-4340-b58f-08194691e10b","process_automation_job_arguments":"-arg 123"},"action_type":"process_automation","creation_time":"2022-12-12T18:51:42.048162Z","description":"Description of Action created by TF","id":"01DF4OBNYKW84FS9CCYVYS1MOS","last_run":"2022-12-12T18:52:11.937747Z","last_run_by":{"id":"PINL781","type":"user_reference"},"modify_time":"2022-12-12T18:51:42.048162Z","name":"Action created by TF","privileges":{"permissions":["read"]},"runner":"01DF4O9T1MDPYOUT7SUX9EXZ4R","runner_type":"runbook","services":[{"id":"PQWQ0U6","type":"service_reference"}],"teams":[{"id":"PZ31N6S","type":"team_reference"}],"type":"action"}}`))
+	})
+
+	resp, _, err := client.AutomationActionsAction.Update(id, input)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	runner_type_runbook := "runbook"
+	modify_time := "2022-12-12T18:51:42.048162Z"
+	permissions_read := "read"
+	resource_type := "action"
+	creation_time := "2022-12-12T18:51:42.048162Z"
+	want := &AutomationActionsAction{
+		ID:           id,
+		Name:         "Action created by TF",
+		Description:  &description,
+		CreationTime: &creation_time,
+		ActionType:   "process_automation",
+		Type:         &resource_type,
+		RunnerID:     &runner_id,
+		RunnerType:   &runner_type_runbook,
+		Teams: []*TeamReference{
+			{
+				Type: "team_reference",
+				ID:   "PZ31N6S",
+			},
+		},
+		Services: []*ServiceReference{
+			{
+				Type: "service_reference",
+				ID:   "PQWQ0U6",
+			},
+		},
+		ActionDataReference: adf,
+		Privileges: &AutomationActionsPrivileges{
+			Permissions: []*string{&permissions_read},
+		},
+		ModifyTime: &modify_time,
+	}
+
+	if !reflect.DeepEqual(resp, want) {
+		t.Errorf("returned \n\n%#v want \n\n%#v", resp, want)
+	}
+}
+
 func TestAutomationActionsActionDelete(t *testing.T) {
 	setup()
 	defer teardown()
